a8b8d01d334034c15f38fae25394530b39b97efd
Pádua Lima <padualima.dev@gmail.com>
2023-02-08 12:30:20 -0300

    create model tests to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_many :api_token_events
   has_many :tweets
+
+  validates :uid, :name, :username, presence: true
+  validates :uid, uniqueness: { case_sensitive: false }
+  validates :username, uniqueness: true
+  validates :uid, format: { with: /\A\d+\d\z/ }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "relations" do
+    it { should have_many(:api_token_events) }
+    it { should have_many(:tweets) }
+  end
+
+  describe "validations" do
+    subject { build(:user) }
+
+    it { should validate_presence_of(:uid) }
+    it { should validate_uniqueness_of(:uid).case_insensitive }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:username) }
+    it { should validate_uniqueness_of(:username) }
+
+    it { should_not allow_value(Faker::Internet.uuid).for(:uid) }
+    it { should allow_value(Faker::Number.number.to_s).for(:uid) }
+    it { should allow_value(Faker::Number.number).for(:uid) }
+  end
 end
