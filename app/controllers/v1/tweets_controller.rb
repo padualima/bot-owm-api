e041eae37d81f95d8df80d59c9c1becc076d53dd
Pádua Lima <padualima.dev@gmail.com>
2023-02-09 19:12:26 -0300

    adding tweets controller and spec request

---
+++ app/controllers/v1/tweets_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module V1
+  class TweetsController < ApiController
+    def create
+    end
+  end
+end
---
+++ spec/requests/v1/tweets_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe "V1::Tweets", type: :request do
+  describe "POST /create" do
+  end
+end
