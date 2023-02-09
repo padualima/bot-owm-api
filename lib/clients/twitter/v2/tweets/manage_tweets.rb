580f96361fe7a3d20685d2a9b4e8ef3762e798c5
Pádua Lima <padualima.dev@gmail.com>
2023-02-09 17:38:07 -0300

    creating method to create a Tweet

---
+++ lib/clients/twitter/v2/tweets/manage_tweets.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Clients
+  module Twitter
+    module V2
+      module Tweets
+        class ManageTweets < Base
+          def new_tweet(text)
+          end
+        end
+      end
+    end
+  end
+end
