1e6f0b695907c0d2b6d9e7174d6e78fa3ede4cca
Pádua Lima <padualima.dev@gmail.com>
2023-02-10 19:37:59 -0300

    create action new tweet with location params

--- app/controllers/v1/tweets_controller.rb
+++ app/controllers/v1/tweets_controller.rb
@@ -2,7 +2,44 @@
 
 module V1
   class TweetsController < ApiController
+    before_action :set_api_token
+
     def create
+      # TODO: check weather location in OpenWeatherMap and change the input text
+      input_location = params[:location].to_s.strip
+
+      if @api_token.nil? || @api_token.expired? || input_location.empty?
+        return head :unprocessable_entity
+      end
+
+      tweet_response = new_tweet_with_text(input_location)
+
+      if tweet_response.status.eql?(200)
+        input = {
+          user_id: @api_token.user.id,
+          api_token_event_id: @api_token.id,
+          uid: tweet_response.body['data']['id'],
+          text: tweet_response.body['data']['text']
+        }
+
+        Tweet
+          .new(input)
+          .then do |tweet|
+            render json: { data: { tweet: { text: tweet.text } } }, status: :ok if tweet.save!
+          end
+      end
+    end
+
+    private
+
+    def set_api_token
+      @api_token = ApiTokenEvent.find_by(token: params[:token])
+    end
+
+    def new_tweet_with_text(text)
+      Clients::Twitter::V2::Tweets::ManageTweets
+        .new(oauth_token: @api_token.access_token)
+        .new_tweet(text)
     end
   end
 end
--- spec/requests/v1/tweets_spec.rb
+++ spec/requests/v1/tweets_spec.rb
@@ -4,5 +4,63 @@ require 'rails_helper'
 
 RSpec.describe "V1::Tweets", type: :request do
   describe "POST /create" do
+    context "when valid params" do
+      let(:api_token_event) { create(:api_token_event) }
+      # TODO: mudar parâmetro quando tiver criado a api OpenWeatherMap
+      let(:location_params) { Faker::Address.city }
+      let(:valid_params) { { token: api_token_event.token, location: location_params } }
+
+      before do
+        allow_any_instance_of(Faraday::Connection).to receive(:post)
+          .and_return(
+            instance_double(
+              Faraday::Response,
+              body: MockTwitterResponse::Tweets.new_tweet_data(text: location_params),
+              status: 200
+            )
+          )
+      end
+
+      it { expect { post tweets_path(params: valid_params) }.to change(Tweet, :count).by(1) }
+    end
+
+    context "when invalid params" do
+      let(:api_token_event) { create(:api_token_event) }
+      let(:location_params) { Faker::Address.city }
+      let(:token_params) { api_token_event.token }
+      let(:invalid_params) { { token: token_params, location: location_params } }
+
+      context "when token has expired" do
+        before do
+          api_token_event.update(expires_in: Time.current - 1.minutes)
+        end
+
+        it do
+          post tweets_path(params: invalid_params)
+
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+
+      context "when token not exist" do
+        let(:token_params) { [nil, "", " "].sample }
+
+        it do
+          post tweets_path(params: invalid_params)
+
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+
+      context "when location is invalid" do
+        let(:location_params) { [nil, "", " "].sample }
+
+        it do
+          post tweets_path(params: invalid_params)
+
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+    end
   end
 end
