06da7f2723778fdb716d282dcefc77e6357eacf6
Pádua Lima <padualima.dev@gmail.com>
2023-07-31 10:19:54 -0300

    add initial service to oauth2  get access token

--- lib/oauth2/client.rb
+++ lib/oauth2/client.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
+require 'json'
+
 module OAuth2
+  AccessTokenResponse = Class.new(Struct.new(:status, :body))
+  ConnectionError = Class.new(Faraday::ConnectionFailed)
+  TimeoutError = Class.new(Faraday::TimeoutError)
+
   class Client
+    GET_TOKEN_ALLOWED_OPTIONS = %i[url body]
+
     attr_reader :client_id, :client_secret, :url
     attr_accessor :options
     attr_writer :connection
@@ -9,7 +17,7 @@ module OAuth2
     # @param [String] client_id the provider client_id key
     # @param [String] client_secret the provider client_secret key
     # @param [String] url the host of the provider
-    # @param [String] options the options of connection builder
+    # @param [Symbol] options the options of connection builder
     def initialize(client_id: nil, client_secret: nil, **options)
       opts = Utils.symbolize_hash_keys(options.dup)
 
@@ -36,6 +44,13 @@ module OAuth2
       connection.build_url(authorize_options_url, authorize_url_params(params)).to_s
     end
 
+    # @param [Hash] params the necessary parameters to make the get token request
+    # @param [Symbol] options allows overriding request options with GET_TOKEN_ALLOWED_OPTIONS
+    def get_token(params = {}, **options)
+      validate_params(params)
+      request(*get_token_options(params, options).keys)
+    end
+
     def connection
       @connection ||= Faraday.new(url) do |client|
         client.request :url_encoded
@@ -54,12 +69,45 @@ module OAuth2
 
     def authorize_options_url = options[:authorize_options][:url]
 
+    def token_options_method = options[:token_options][:method]
+
+    def token_options_url = options[:token_options][:url]
+
+    def token_options_headers = options[:token_options][:headers]
+
     def redirect_uri_params = redirect_uri.present? ? { redirect_uri: redirect_uri } : {}
 
     def authorize_url_params(params = {})
       redirect_uri_params
         .merge(Utils.symbolize_hash_keys(params))
         .then { |params| Utils.stringify_hash_keys(params) }
+    end
+
+    def get_token_options(params = {}, options = {})
+      {
+        method: token_options_method,
+        url: token_options_url,
+        body: params,
+        headers: token_options_headers
+      }
+        .merge(Utils.filter_hash_by_keys(options, GET_TOKEN_ALLOWED_OPTIONS))
+        .tap { |opts| opts[:body] = Utils.stringify_hash_keys(opts[:body]) }
+    end
+
+    def request(method, url, body, headers)
+      response = run_request(method, url, body, headers)
+
+      return response unless response&.status&.in?(200..599)
+
+      AccessTokenResponse.new(status: response.status, body: JSON.parse(response.body))
+    end
+
+    def run_request(method, url, body, headers)
+      connection.run_request(method, url, body, headers)
+    rescue Faraday::ConnectionFailed => e
+      raise ConnectionError, e
+    rescue Faraday::TimeoutError => e
+      raise TimeoutError, e
     end
   end
 end
--- spec/lib/oauth2/client_spec.rb
+++ spec/lib/oauth2/client_spec.rb
@@ -9,19 +9,32 @@ RSpec.describe OAuth2::Client do
   let(:callback_uri) { 'http://example.com/auths/provider/callback' }
   let(:options) { { url: provider_host, redirect_uri: callback_uri } }
   let(:authorize_options) { { url: 'oauth/authorize' } }
+  let(:token_options) do
+    {
+      method: :post,
+      url: 'oauth/token',
+      headers: { 'Content-Type' => 'application/x-www-form-urlencoded' }
+    }
+  end
 
   subject { described_class.new(client_id: client_id, client_secret: client_secret, **options) }
+
+  describe 'Constants' do
+    it 'make sure the constant GET_TOKEN_ALLOWED_OPTIONS is defined' do
+      expect(defined?(OAuth2::Client::GET_TOKEN_ALLOWED_OPTIONS)).to be_truthy
+    end
+
+    it 'checks the value of the constant GET_TOKEN_ALLOWED_OPTIONS' do
+      expect(OAuth2::Client::GET_TOKEN_ALLOWED_OPTIONS).to eq(%i[url body])
+    end
+  end
 
   describe '#initialize' do
     let(:default_options) do
       {
         redirect_uri: nil,
         authorize_options: authorize_options,
-        token_options: {
-          method: :post,
-          url: 'oauth/token',
-          headers: { 'Content-Type' => 'application/x-www-form-urlencoded' }
-        },
+        token_options: token_options,
         logger: ::Logger.new($stdout)
       }
     end
@@ -109,6 +122,26 @@ RSpec.describe OAuth2::Client do
     end
   end
 
+  describe '#get_token' do
+    let(:params) { { code: 1234, grant_type: 'authorization_code' } }
+
+    it 'returns with access_token response' do
+      mock_response = instance_double(Faraday::Response, body: params.to_json, status: 200)
+
+      allow(subject.connection).to receive(:run_request).and_return(mock_response)
+
+      response = subject.get_token(params)
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq(params.as_json)
+    end
+
+    it 'raise an Exception when invalid params' do
+      expect { subject.get_token('') }
+        .to raise_error(ArgumentError, '`params` is expected to be a Hash')
+    end
+  end
+
   describe '#connection' do
     it 'is an instance of faraday with url from provider' do
       expect(subject.connection).to be_a(Faraday::Connection)
@@ -118,6 +151,34 @@ RSpec.describe OAuth2::Client do
 
   describe '#redirect_uri' do
     it { expect(subject.send(:redirect_uri)).to eq(subject.options[:redirect_uri]) }
+  end
+
+  describe '#authorize_options_url' do
+    it do
+      expect(subject.send(:authorize_options_url))
+        .to eq(subject.options[:authorize_options][:url])
+    end
+  end
+
+  describe '#token_options_method' do
+    it do
+      expect(subject.send(:token_options_method))
+        .to eq(subject.options[:token_options][:method])
+    end
+  end
+
+  describe '#token_options_url' do
+    it do
+      expect(subject.send(:token_options_url))
+        .to eq(subject.options[:token_options][:url])
+    end
+  end
+
+  describe '#token_options_headers' do
+    it do
+      expect(subject.send(:token_options_headers))
+        .to eq(subject.options[:token_options][:headers])
+    end
   end
 
   describe '#redirect_uri_params' do
@@ -150,6 +211,138 @@ RSpec.describe OAuth2::Client do
       subject.options.delete(:redirect_uri)
 
       expect(subject.send(:authorize_url_params, params)).to eq(params.transform_keys(&:to_s))
+    end
+  end
+
+  describe '#get_token_options' do
+    let(:code) { '12345' }
+    let(:params) { { code: code } }
+    let(:get_token_default_options) do
+      {
+        method: token_options[:method],
+        url: token_options[:url],
+        body: params.transform_keys(&:to_s),
+        headers: token_options[:headers]
+      }
+    end
+
+    it 'returns defaults options without options args' do
+      expect(subject.send(:get_token_options, params)).to eq(get_token_default_options)
+      expect(subject.send(:get_token_options, params).keys).to all(be_a(Symbol))
+      expect(subject.send(:get_token_options, params)[:body].keys).to all(be_a(String))
+    end
+
+    it 'returns with replaced URL and body' do
+      replaced_options = {
+        url: 'http://another.example.com/auths/oauth/tokens',
+        body: { code: code, grant_type: 'authorization_code' }
+      }
+
+      get_token_default_options[:url] = replaced_options[:url]
+      get_token_default_options[:body] = replaced_options[:body].transform_keys(&:to_s)
+
+      expect(subject.send(:get_token_options, params, replaced_options))
+        .to eq(get_token_default_options)
+      expect(subject.send(:get_token_options, params, replaced_options).keys)
+        .to all(be_a(Symbol))
+      expect(subject.send(:get_token_options, params, replaced_options)[:body].keys)
+        .to all(be_a(String))
+    end
+  end
+
+  describe '#request' do
+    let(:method) { :post }
+    let(:url) { 'http://example.com/' }
+    let(:body) { URI.encode_www_form({ 'param_1' => 'value_1' }) }
+    let(:headers) { { 'Content-Type' => 'application/json' } }
+
+    it 'returns an object of AccessTokenResponse when request is successful' do
+      mock_response =
+        instance_double(Faraday::Response, body: { token: 'Token' }.to_json, status: 200)
+
+      allow(subject.connection).to receive(:run_request).and_return(mock_response)
+
+      response = subject.send(:request, method, url, body, headers)
+
+      expect(response.status).to eq(200)
+      expect(response.body).to have_key('token')
+      expect(response.body['token']).to eq('Token')
+    end
+
+    context 'when return Connection Errors' do
+      before  { allow(subject.connection).to receive(:run_request).and_raise(faraday_exception) }
+
+      shared_examples 'failed connection handler' do
+        it 'rescues the exception' do
+          expect { subject.send(:request, method, url, body, headers) }
+            .to raise_error do |e|
+              expect(e.message).to eq(faraday_exception.message)
+              expect(e.class).to eq(expected_exception)
+            end
+        end
+      end
+
+      context 'returns an Faraday::ConnectionFailed' do
+        let(:faraday_exception) { Faraday::ConnectionFailed.new('fail') }
+        let(:expected_exception) { OAuth2::ConnectionError }
+
+        it_behaves_like 'failed connection handler'
+      end
+
+      context 'returns an Faraday::TimeoutError' do
+        let(:faraday_exception) { Faraday::TimeoutError.new('timeout') }
+        let(:expected_exception) { OAuth2::TimeoutError }
+
+        it_behaves_like 'failed connection handler'
+      end
+    end
+  end
+
+  describe '#run_request' do
+    let(:method) { :post }
+    let(:url) { 'http://example.com/' }
+    let(:body) { URI.encode_www_form({ 'param_1' => 'value_1' }) }
+    let(:headers) { { 'Content-Type' => 'application/json' } }
+
+    it 'returns with success response' do
+      mock_response =
+        instance_double(Faraday::Response, body: { token: 'Token' }.to_json, status: 200)
+
+      allow(subject.connection).to receive(:run_request).and_return(mock_response)
+
+      response = subject.send(:run_request, method, url, body, headers)
+
+      expect(response.status).to eq(200)
+      expect(JSON.parse(response.body)).to have_key('token')
+      expect(JSON.parse(response.body)['token']).to eq('Token')
+    end
+
+    context 'when errors are raised by Faraday' do
+      before  { allow(subject.connection).to receive(:run_request).and_raise(faraday_exception) }
+
+      shared_examples 'failed connection handler' do
+        it 'rescues the exception' do
+          expect { subject.send(:run_request, method, url, body, headers) }
+            .to raise_error do |e|
+              expect(e.message).to eq(faraday_exception.message)
+              expect(e.class).to eq(expected_exception)
+            end
+        end
+      end
+
+      context 'with Faraday::ConnectionFailed' do
+        let(:faraday_exception) { Faraday::ConnectionFailed.new('fail') }
+        let(:expected_exception) { OAuth2::ConnectionError }
+
+        it_behaves_like 'failed connection handler'
+      end
+
+      context 'with Faraday::TimeoutError' do
+        let(:faraday_exception) { Faraday::TimeoutError.new('timeout') }
+        let(:expected_exception) { OAuth2::TimeoutError }
+
+        it_behaves_like 'failed connection handler'
+      end
     end
   end
 end
