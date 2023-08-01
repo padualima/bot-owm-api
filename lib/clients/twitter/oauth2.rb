adf7e199e9ac760e5e6eef897314c8f7879eea71
Pádua Lima <padualima.dev@gmail.com>
2023-08-01 01:37:07 -0300

    trash code removed

--- lib/clients/twitter/oauth2.rb
+++ lib/clients/twitter/oauth2.rb
@@ -8,21 +8,6 @@ module Clients
     class OAuth2 < V2::Base
       include Utils
 
-      def authorize_url
-        params = {
-          client_id: client_id,
-          redirect_uri: callback_url,
-          state: code_verifier,
-          code_challenge: code_challenge,
-          scope: scopes,
-          response_type: 'code',
-          code_challenge_method: "S256"
-        }
-        .map { |k,v| [k,v].join('=') }
-        .join('&')
-        .then { |params| "#{ENV['TWITTER_AUTHORIZE_URL']}?#{params}" }
-      end
-
       def access_token(state, code)
         call(
           method: :post,
