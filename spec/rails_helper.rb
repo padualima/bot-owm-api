abeb9ead1be4024af2bca2de4eebbc64160e8549
Pádua Lima <padualima.dev@gmail.com>
2023-08-13 02:27:24 -0300

    adding gem webmock

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,7 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-require "rspec/json_expectations"
+require 'rspec/json_expectations'
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
